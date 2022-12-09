c5d11617a833b5eec6ac8f215cdbaae1f5baebf6
ValeriySpatari <smokeyou92@gmail.com>
2022-12-09 18:56:31 +0200

    add hw2 for seminar 2

---
+++ HW_seminar_2/Program.cs
@@ -0,0 +1,63 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.*/
+
+// Console.WriteLine("Введите трёхзначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// void Tripopotam()
+// {
+// if (number >= 100 && number < 1000)
+//     Console.WriteLine((number % 100) / 10);
+// else
+//     Console.WriteLine("Введите правильное число!");
+// }
+
+// Console.Write("Вторая цифра этого числа: ");
+// Tripopotam();
+
+
+
+/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.*/
+
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// void ThirdNumber(int number)
+// {
+//     while(number > 999)
+//     {
+//         number = number/10;
+//     }
+//     if(number < 999 & number > 100)
+//     {
+//         number %= 10;
+//         Console.WriteLine(number);
+//     }
+//     else if (number < 100)
+//     {
+//         Console.WriteLine("Третьей цифры нет");
+//     }
+    
+// }
+
+// ThirdNumber(number);
+
+
+/*Задача 15: Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.*/
+
+// Console.WriteLine("Введите цифру от 1 до 7: ");
+// int day = Convert.ToInt32(Console.ReadLine());
+
+// void DayOfWeek (int day)
+// {
+//     if(day == 6 || day == 7) Console.WriteLine("Поздравляшки! Это выходной!");
+
+//     else if (day > 7 || day < 1) Console.WriteLine("Внимательнее! Введите число в указанном диапазоне.");
+//     else Console.WriteLine("К сожалению, это будний день..");
+
+
+// }
+
+// DayOfWeek(day);
